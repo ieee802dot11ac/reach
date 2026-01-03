d57265aef5550ebcae7ae72846d390ea6682894e
ieee 802.11ac <69217234+ieee802dot11ac@users.noreply.github.com>
2026-01-02 21:47:36 -0800

    init

---
+++ tools/defines_common.py
@@ -0,0 +1,14 @@
+# Game versions
+DEFAULT_VERSION = 0
+VERSIONS = [
+    "4D53085B",  # 0
+]
+
+# Include paths
+cflags_includes = [
+    # This is where you'd put common include paths for your project
+    # So for example, if I'd want my project to search for headers from "src/system",
+    # I'd put "/I src/system" here
+    "/I src",
+    "/I src/xdk"
+]
